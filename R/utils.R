53f3f64406e21ea7b08efd6fed4b30117076cd4d
Christoph Hafemeister <chafemeister@nygenome.org>
2018-09-12 12:47:44 +0200

    Modifications to how we detect outliers before parameter regularization

--- R/utils.R
+++ R/utils.R
@@ -2,31 +2,43 @@
 #'
 #' @param y Dependent variable
 #' @param x Independent variable
-#' @param n Number of observations per bin
 #' @param th Outlier score threshold
 #'
 #' @return Boolean vector
 #'
 #' @importFrom stats aggregate
 #'
-is_outlier <- function(y, x, n, th = 40) {
-  o <- order(x)
-  x <- x[o]
-  y <- y[o]
-  n.bins <- ceiling(length(x) / n)
-  if (n.bins > 1) {
-    bins <- cut(rank(x, ties.method = 'first'), n.bins, ordered_result = TRUE)
-  } else {
-    bins <- rep(1, length(y))
-  }
+is_outlier <- function(y, x, th = 10) {
+  #bin.width <- var(x) * bw.SJ(x)
+  bin.width <- (max(x) - min(x)) * bw.SJ(x) / 2
+  eps <- .Machine$double.eps * 10
+  breaks1 <- seq(from = min(x) - eps, to = max(x) + bin.width, by = bin.width)
+  breaks2 <- seq(from = min(x) - eps - bin.width/2, to = max(x) + bin.width, by = bin.width)
+  score1 <- robust_scale_binned(x, breaks1)
+  score2 <- robust_scale_binned(x, breaks2)
+  return(pmin(abs(score1), abs(score2)) > th)
+}
+
+#' Robust scale using median and mad per bin
+#'
+#' @param x Numeric vector
+#' @param breaks Numeric vector of breaks
+#'
+#' @return Numeric vector of scaled score
+#'
+#' @importFrom stats aggregate
+#'
+robust_scale_binned <- function(x, breaks) {
+  bins <- cut(x = x, breaks = breaks, ordered_result = TRUE)
   tmp <- aggregate(x = y, by = list(bin=bins), FUN = robust_scale)
   score <- rep(0, length(x))
+  o <- order(bins)
   if (class(tmp$x) == 'list') {
     score[o] <- unlist(tmp$x)
   } else {
     score[o] <- as.numeric(t(tmp$x))
   }
-  return(score > th)
+  return(score)
 }
 
 #' Robust scale using median and mad
