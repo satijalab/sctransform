d4db7783be10e939bb8c3940ac0afa4da6a2f302
Christoph Hafemeister <chafemeister@nygenome.org>
2019-03-15 11:22:12 +0100

    Update tests

--- tests/testthat/test_denoising.R
+++ tests/testthat/test_denoising.R
@@ -7,11 +7,11 @@ test_that('correcting runs and returns expected output', {
   vst_out <- vst(pbmc, return_cell_attr = TRUE, res_clip_range = c(-Inf, Inf))
   y_smooth <- smooth_via_pca(vst_out$y, do_plot = FALSE)
   expect_equal(c(910, 283), dim(y_smooth))
-  expect_equal(c(0.06362666, -0.01528845,  0.84735003,  0.37468399,
-                 -0.03071016, 1.34827719, -0.01143410, -0.29820178,
-                 -0.64414609), as.numeric(y_smooth[1:3, 1:3]))
+  expect_equal(c(0.05809, -0.00707,  0.55978,  0.27358,
+                 -0.01979, 0.83436, 0.03495, -0.09587,
+                 -0.88417), as.numeric(y_smooth[1:3, 1:3]), tolerance = 1e-5)
   umi_corrected <- correct(vst_out)
-  expect_equal(c(0, 1, 27, 1, 1, 36, 0, 0, 7), as.numeric(umi_corrected[1:3, 1:3]))
+  expect_equal(c(0, 1, 28, 1, 1, 37, 0, 0, 7), as.numeric(umi_corrected[1:3, 1:3]))
   umi_corrected <- correct(vst_out, data = y_smooth)
-  expect_equal(c(0, 0, 28, 0, 0, 34, 0, 0, 9), as.numeric(umi_corrected[1:3, 1:3]))
+  expect_equal(c(0, 0, 30, 0, 0, 34, 0, 0, 9), as.numeric(umi_corrected[1:3, 1:3]))
 })
--- tests/testthat/test_vst.R
+++ tests/testthat/test_vst.R
@@ -5,8 +5,8 @@ test_that('vst runs and returns expected output', {
   options(mc.cores = 2)
   set.seed(42)
   vst_out <- vst(pbmc, return_gene_attr = TRUE, return_cell_attr = TRUE)
-  expect_equal(c(9727, 402), dim(vst_out$y))
+  expect_equal(c(910, 283), dim(vst_out$y))
   ga <- vst_out$gene_attr[order(-vst_out$gene_attr$residual_variance), ]
-  expect_equal(c("PPBP", "GNLY", "IGLL5", "PF4", "GZMB"), rownames(ga)[1:5])
-  expect_equal(c(40.30323, 32.55276, 20.85651, 20.02775, 18.82615), ga$residual_variance[1:5], tolerance = 1e-05)
+  expect_equal(c("GNLY", "NKG7", "GZMB", "LYZ", "S100A9"), rownames(ga)[1:5])
+  expect_equal(c(27.8, 26.9, 18.7, 18.2, 16.8), ga$residual_variance[1:5], tolerance = 1e-01)
 })
