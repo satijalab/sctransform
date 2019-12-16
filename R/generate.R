5d159d479989176ba0588ec3e7d042a5f52ec193
Christoph Hafemeister <chafemeister@nygenome.org>
2019-12-16 22:35:43 +0100

    Fix function to generate data
    Cell attributes are sampled from input and used with regularized models to generate synthetic data. Pearson residuals are ignored.

--- R/generate.R
+++ R/generate.R
@@ -1,60 +1,43 @@
 #' Generate data from regularized models.
 #'
-#' Generate data from regularized models. If background_only is set to TRUE no residuals are
-#' added to the simulated data.
+#' Generate data from regularized models. This generates data from the background,
+#' i.e. no residuals are added to the simulated data. The cell attributes for the
+#' generated cells are sampled from the input with replacment.
 #'
 #' @param vst_out A list that provides model parameters and optionally meta data; use output of vst function
-#' @param data The name of the entry in x that holds the data (Pearson residuals)
-#' @param cell_attr Provide cell meta data holding latent data info
-#' @param background_only Set to TRUE to only generate the background signal (do not add back residuals)
-#' @param do_round Round the result to integers
-#' @param do_pos Set negative values in the result to zero
-#' @param make_sparse Make the output a dgCMatrix (if do_pos and do_round are also TRUE)
+#' @param genes The gene names for which to generate data; default is rownames(vst_out$model_pars_fit)
+#' @param cell_attr Provide cell meta data holding latent data info; default is vst_out$cell_attr
+#' @param n_cells Number of cells to generate; default is nrow(cell_attr)
 #'
-#' @return Generated data
+#' @return Generated data as dgCMatrix
+#'
+#' @importFrom methods as
 #'
 #' @export
 #'
 #' @examples
-#' vst_out <- vst(pbmc, return_cell_attr = TRUE, res_clip_range = c(-Inf, Inf))
+#' vst_out <- vst(pbmc, return_cell_attr = TRUE)
 #' generated_data <- generate(vst_out)
 #'
-generate <- function(vst_out, data = 'y', genes = rownames(vst_out$model_pars_fit),
-                     cell_attr = vst_out$cell_attr, background_only = FALSE,
-                     do_round = FALSE, do_pos = FALSE,
-                     make_sparse = FALSE) {
+generate <- function(vst_out, genes = rownames(vst_out$model_pars_fit),
+                     cell_attr = vst_out$cell_attr,
+                     n_cells = nrow(cell_attr)) {
   genes <- genes[genes %in% rownames(vst_out$model_pars_fit)]
-  # when we generate data, use the cell attributes from the original data
-  if (is.character(data)) {
-    data <- vst_out[[data]]
-  }
-  data <- data[genes, , drop = FALSE]
   # get model parameters
   mp <- vst_out$model_pars_fit[genes, , drop = FALSE]
   coefs <- mp[, -1, drop=FALSE]
   theta <- mp[, 1]
-  ca <- cell_attr
-  regressor_data <- cbind(rep(1, nrow(ca)), ca[, colnames(coefs)[-1]])
+  # we sample from the original list of cell attributes when we generate data
+  # choose cells here
+  idx <- sample(x = nrow(cell_attr), size = n_cells, replace = TRUE)
+  regressor_data <- cbind(rep(1, length(idx)), cell_attr[idx, colnames(coefs)[-1]])
   # calculate expected values
   mu <- exp(tcrossprod(coefs, regressor_data))
   x.sim <- t(sapply(rownames(mu), function(gene) {
     gene.mu <- mu[gene, ]
     x <- MASS::rnegbin(n = length(gene.mu), mu = gene.mu, theta = theta[gene])
-    if (!background_only) {
-      this.sd <- sqrt(gene.mu + gene.mu^2 / theta[gene])
-      # add back residuals
-      x <- x + this.sd * data[gene, ]
-    }
     return(x)
   }))
-  if (do_round) {
-    x.sim <- round(x.sim, 0)
-  }
-  if (do_pos) {
-    x.sim[x.sim < 0] <- 0
-  }
-  if (do_round & do_pos & make_sparse) {
-    x.sim <- as(x.sim, Class = 'dgCMatrix')
-  }
+  x.sim <- as(x.sim, Class = 'dgCMatrix')
   return(x.sim)
 }
