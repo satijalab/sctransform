02f915b64f7f27f46cf8c26bfa22f7510d1fae05
yuhanH <yh1970@nyu.edu>
2020-08-19 12:04:32 -0400

    suggest glmgampoi, and add gene bin

--- R/vst.R
+++ R/vst.R
@@ -66,7 +66,6 @@ NULL
 #' @importFrom stats glm ksmooth model.matrix as.formula approx density poisson var bw.SJ
 #' @importFrom utils txtProgressBar setTxtProgressBar capture.output
 #' @importFrom methods as
-#' @importFrom glmGamPoi glm_gp
 #'
 #' @export
 #'
@@ -183,7 +182,7 @@ vst <- function(umi,
 
   model_pars <- get_model_pars(genes_step1, bin_size, umi, model_str, cells_step1, method, data_step1, theta_given, show_progress)
   
-  # DEAL WITH 0 VALUES
+  # DEAL WITH Inf theta from glmGamPoi
   model_pars <- model_pars[model_pars[,1]!=Inf,]
   genes_step1 <- rownames(model_pars)
   genes_log_gmean_step1 <- genes_log_gmean_step1[genes_step1]
@@ -325,17 +324,23 @@ get_model_pars <- function(genes_step1, bin_size, umi, model_str, cells_step1, m
     pb <- txtProgressBar(min = 0, max = max_bin, style = 3)
   }
   model_pars <- list()
-  if (method == 'glmGamPoi') {
-    model_matrix <- cbind(1, data_step1$log_umi)
-    fit <- glm_gp(data = as.matrix(umi[genes_step1,cells_step1,drop=FALSE]), 
-                  design = model_matrix, 
-                  size_factors = FALSE)
-    model_pars <- as.matrix(data.frame(cbind(1 / fit$overdispersions),fit$Beta))
-    rownames(model_pars) <- genes_step1
-    colnames(model_pars) <- c('theta', '(Intercept)',"log_umi")
-    return(model_pars)
-  }
   
+  # package checking
+  if (method == "glmGamPoi") {
+    glmGamPoi.check <- PackageCheck("glmGamPoi", error = FALSE)
+    if (!glmGamPoi.check){
+      message(
+        "For a more efficient implementation of the sctransform,",
+        "\n please install the glmGamPoi package",
+        "\n--------------------------------------------",
+        "\ninstall.packages('BiocManager')",
+        "\nBiocManager::install('glmGamPoi')",
+        "\n--------------------------------------------",
+        "\nmethod is set to poisson until glmGamPoi is installed"
+      )
+      method <- "poisson"
+    }
+  }
   for (i in 1:max_bin) {
     genes_bin_regress <- genes_step1[bin_ind == i]
     umi_bin <- as.matrix(umi[genes_bin_regress, cells_step1, drop=FALSE])
@@ -380,8 +385,16 @@ get_model_pars <- function(genes_step1, bin_size, umi, model_str, cells_step1, m
                                      return(c(fit$theta, fit$coefficients))
                                    }
                                    if (method == "glmGamPoi") {
-                                     
-                                     
+                                     fit <- glmGamPoi::glm_gp(data = y, 
+                                                  design = as.formula(gsub("y","",model_str)), 
+                                                  col_data = data_step1,
+                                                   size_factors = FALSE)
+                                     theta <- cbind(1 / fit$overdispersions)
+                                     model_pars <- as.matrix(data.frame(theta, fit$Beta))
+                                     colnames(model_pars) <- gsub(pattern = "Intercept",
+                                                                  replacement = "(Intercept)", 
+                                                                  x = colnames(model_pars))
+                                     return(model_pars)
                                    }
                                  }
                                )
