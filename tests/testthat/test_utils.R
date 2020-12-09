c7994239e5fac503d22206e198c5e30f2dac9e46
Christoph Hafemeister <christoph.hafemeister@nyu.edu>
2020-12-09 23:12:21 +0100

    Add tests for row_nonzero_count

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -43,3 +43,25 @@ test_that('row_mean_grouped runs and returns expected output', {
   
   expect_equal(means, means_agg)
 })
+
+test_that('row_nonzero_count runs and returns expected output', {
+  skip_on_cran()
+  suppressWarnings(RNGversion(vstr = "3.5.0"))
+  set.seed(42)
+  
+  nzc <- sctransform:::row_nonzero_count_dgcmatrix(pbmc)
+  nzc2 <- Matrix::rowSums(pbmc > 0)
+  expect_equal(nzc, nzc2)
+  
+  grouping <- as.factor(sample(c('a','b','c'), size = ncol(pbmc), replace = TRUE))
+  nzc <- sctransform:::row_nonzero_count_grouped_dgcmatrix(pbmc, grouping)
+  f2 <- function(mat, grp) {
+    ret <- sapply(levels(grp), function(g) {
+      rowSums(mat[, grp == g, drop = FALSE] > 0)
+    })
+    colnames(ret) <- levels(grp)
+    ret
+  }
+  nzc2 <- f2(pbmc, grouping)
+  expect_equal(nzc, nzc2)
+})
